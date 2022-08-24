02a51a56ddda28811431e69e53c9f1aa02e07a59
unknown <riswantosira@gmail.com>
2022-08-24 21:50:22 +0700

    tambah button

--- resources/views/categories/asw.blade.php
+++ resources/views/categories/asw.blade.php
@@ -1,3 +1,6 @@
 <h3>Agung Riswanto</h3>
 
 <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae corporis ut id impedit esse. Nihil alias at doloribus. Eos, fugit.</h4>
+
+
+<button>AAWOKAOWKAOKWAOKWAOKWAO</button>
